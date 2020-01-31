e5c014deedabeaf1876d30628fc0bc8c198a9a33
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2020-01-31 08:22:59 -0600

    exponential growth

---
+++ exponential_prediction.R
@@ -0,0 +1,31 @@
+options(scipen = 999)
+date <- seq(as.Date("2020/01/21"), by = "day", length.out = 10)
+data <- data.frame(time = c(1:10),
+                   case = c(278, 309, 571, 830, 1297, 1958, 2741, 4537, 6065, 7736))
+
+exponential.model <- lm(log(data$case)~data$time)
+exponential.model
+
+timevalues <- c(1:10)
+Counts.exponential2 <- exp(predict(exponential.model, list(time=timevalues)))
+plot(data$time, data$case, pch=16, las =1)
+lines(timevalues, Counts.exponential2, lwd=2, col = "red", xlab = "Time (s)", ylab = "Counts")
+
+gr1 <- exponential.model$coefficients[1]
+gr2 <- exponential.model$coefficients[2]
+
+
+time <- c(1:24)
+case <- exp(gr2*time)
+plot(time, case, type="l", las=1, xlab = "Disease start time (day)", ylab = "Total confirmed cases", col="blue")
+case <- exp(gr2*1.1*time)
+lines(time, case, type="l", las=1, col="blue", lty=2)
+case <- exp(gr2*0.9*time)
+lines(time, case, type="l", las=1, col="blue", lty=2)
+abline(h=10000, lty=3)
+abline(h=1000, lty=3)
+
+axis(3, 1:10, date)
+lines(timevalues, Counts.exponential2, lwd=2, col = "red")
+points(data$time, data$case, pch=16, las=1)
+
