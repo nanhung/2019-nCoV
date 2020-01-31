46d79291f21a9a327b82e4c55f0df2bfb38c70e4
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2020-01-31 06:43:42 -0600

    data update

--- 2019-nCoV_incubation-origin.R
+++ 2019-nCoV_incubation-origin.R
@@ -1,8 +1,16 @@
+# This supplementary material is hosted by Eurosurveillance as supporting information
+# alongside the article “The incubation period of 2019-nCoV infections among travellers from
+# Wuhan, China”, on behalf of the authors, who remain responsible for the accuracy and
+# appropriateness of the content. The same standards for ethics, copyright, attributions and
+# permissions as for the article apply. Supplements are not edited by Eurosurveillance and the
+# journal is not responsible for the maintenance of any links or email addresses provided
+# therein.
+
 #########################################################################################
 # Incubation period estimation for 2019-nCoV
-# based on 34 confirmed cases outside of Wuhan in the period 20-23 January 2020
-# with known travel history and symptom onset date
-# 27 January 2020
+# based on 88 confirmed cases outside of Wuhan in the period 20-28 January 2020
+# with known travel history from Wuhan,China and symptom onset date
+# 29 January 2020
 # Jantien Backer (jantien.backer@rivm.nl)
 #########################################################################################
 
@@ -11,25 +19,26 @@ library(rstan)
 library(loo)
 
 rstan_options(auto_write = TRUE)
-options(mc.cores = 10)
+options(mc.cores = 8)
 
 data <- read_tsv(file = "paper_data.tsv")
 
-tSymptomOnset <- as.integer((data$`Symptom onset` %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31"))
-tStartExposure <- as.integer((data$`Start exposure` %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31"))
-tEndExposure <- as.integer((data$`End exposure` %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31"))
+data <- data %>% 
+  mutate(tReport = as.integer((`reporting date` %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31")),
+         tSymptomOnset = as.integer((symptom_onset %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31")),
+         tStartExposure = as.integer((exposure_start %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31")),
+         tEndExposure = as.integer((exposure_end %>% as.Date(format = "%m/%d/%Y")) - as.Date("2019-12-31"))) %>%
+  # if no start of exposure (i.e. Wuhan residents) use arbitrarily chosen start exposure in far away in past (here half December 2019)
+  mutate(tStartExposure = ifelse(is.na(tStartExposure), min(tSymptomOnset)-21, tStartExposure)) %>%
+  # if symptom onset in Wuhan, exposure ends at symptom onset
+  mutate(tEndExposure = ifelse(tEndExposure > tSymptomOnset, tSymptomOnset, tEndExposure))
 
-# exposure should ends before symptom onset
-tEndExposure[tSymptomOnset < tEndExposure] <- tSymptomOnset[tSymptomOnset < tEndExposure]
-# cases without maximum incubation period get start of exposure 21 days prior to first symptom onset
-tStartExposure[is.na(tStartExposure)] <- min(tSymptomOnset) - 21
-
-# input.data for stan analysis
 input.data <- list(
   N = nrow(data),
-  tStartExposure = tStartExposure,
-  tEndExposure = tEndExposure,
-  tSymptomOnset = tSymptomOnset)
+  tStartExposure = data$tStartExposure,
+  tEndExposure = data$tEndExposure,
+  tSymptomOnset = data$tSymptomOnset)
+
 
 # compile model
 
@@ -72,7 +81,9 @@ generated quantities {
 
 stanfit <- stan(fit = model, data = input.data, 
                 init = "random",
-                iter = 10000, chains = 8)
+                warmup = 4000,
+                iter = 14000, 
+                chains = 8)
 
 
 # check results and convergence
@@ -95,3 +106,4 @@ quantile(sqrt(sigma^2*(gamma(1+2/alpha)-(gamma(1+1/alpha))^2)), probs = c(0.025,
 # posterior median and 95%CI of percentiles
 percentiles <- sapply(c(0.025, 0.05, 0.5, 0.95, 0.975, 0.99), function(p) quantile(qweibull(p = p, shape = alpha, scale = sigma), probs = c(0.025, 0.5, 0.975)))
 colnames(percentiles) <- c(0.025, 0.05, 0.5, 0.95, 0.975, 0.99)
+
